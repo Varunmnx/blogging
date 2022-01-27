efe2c86dd3f68609fc3658ccef18fa0627a4a5a9
Varunmnx <varun.cheriyath@gmail.com>
2022-01-28 00:25:34 +0530

    added userauth

--- blogger/app1/urls.py
+++ blogger/app1/urls.py
@@ -2,8 +2,14 @@ from unicodedata import name
 from django.urls import path,include
 from . import views 
 from users import views as user_views
+from django.contrib.auth import views as auth_views
+
+
 urlpatterns = [
 path('',views.home,name='blog-home'),
 path('about/',views.about,name='blog-about'),
 path('register/',user_views.register,name='register'),
+path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
+path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
+
 ]
--- blogger/blogger/settings.py
+++ blogger/blogger/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app1',
+    'crispy_forms',
     'users.apps.UsersConfig',
 ]
 
@@ -126,3 +127,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
