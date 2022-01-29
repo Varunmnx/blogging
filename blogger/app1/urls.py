618577ede2cef03a1722e60959e3a2b27dad1834
Varunmnx <varun.cheriyath@gmail.com>
2022-01-29 16:00:49 +0530

    added prfile feature

--- blogger/app1/urls.py
+++ blogger/app1/urls.py
@@ -1,11 +1,15 @@
 from unicodedata import name
 from django.urls import path,include
+
+from blogger.settings import DEBUG, MEDIA_ROOT
 from . import views 
 from users import views as user_views
 from django.contrib.auth import views as auth_views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+path('profile/',user_views.profile,name='profile'),
 path('',views.home,name='blog-home'),
 path('about/',views.about,name='blog-about'),
 path('register/',user_views.register,name='register'),
@@ -13,3 +17,6 @@ path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),nam
 path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
 
 ]
+
+if settings.DEBUG :
+    urlpatterns+= static(settings.MEDIA_URL,document_root =settings.MEDIA_ROOT)
--- blogger/users/views.py
+++ blogger/users/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm 
 from django.contrib import messages
+from importlib_metadata import email
 from .forms import UserRegistrationForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def register(request):
    
@@ -10,11 +12,16 @@ def register(request):
         if form.is_valid():
             form.save()
             username= form.cleaned_data.get('username')
-            messages.success(request,f'Account Created for {username}!')
-            return redirect('blog-home')   
+            email= form.cleaned_data.get('email')
+            messages.success(request,f'Your account has been created !! Now you can login')
+            return redirect('login')   
         else:
             messages.warning(request,f'invalid credentials')       
     else:
         form =UserRegistrationForm()
 
     return render(request,'users/register.html',{'form':form})
+
+@login_required()
+def profile(request):
+    return render(request,'users/profile.html')    
