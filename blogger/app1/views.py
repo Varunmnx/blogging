f6e8bb1cae50bb941bef4d5f24ecdf773f569f02
Varunmnx <varun.cheriyath@gmail.com>
2022-02-02 21:11:00 +0530

    added cred operation

--- blogger/app1/views.py
+++ blogger/app1/views.py
@@ -5,7 +5,7 @@ from unicodedata import name
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
-from django.views.generic import ListView,DetailView,CreateView,UpdateView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 # Create your views here.
 
@@ -44,6 +44,17 @@ class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
         if self.request.user == post.author:
             return True
         return False        
+
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Post
+    success_url = '/'
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False        
+
 
 
 def about(request):
